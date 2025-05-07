5dafa0b28423682f4d7b86ec955fc1efb2958a4c
Ayren King <ashsquads@gmail.com>
2025-05-07 10:48:17 -0500

    Update to generate split enums based on string keys

--- L10nXcstrings.py
+++ L10nXcstrings.py
@@ -75,7 +75,7 @@ def find_used_keys_in_code(source_dir, keys, enum_name):
     return used_keys
 
 def find_unused_keys(args, swiftified_keys):
-    defined_keys = set(swiftified_keys.keys())
+    defined_keys = set(swiftified_keys)
     used_keys = find_used_keys_in_code(args.source_dir, defined_keys, args.enum_name)
     unused = defined_keys - used_keys
 
@@ -87,7 +87,7 @@ def find_unused_keys(args, swiftified_keys):
         with open(args.output_unused, "w", encoding="utf-8") as f:
             for key in sorted(unused):
                 print(f" - {key}")
-                f.write(f"{swiftified_keys[key]}\n")
+                f.write(f"{key}\n")
 
         print(f"\nTotal: {len(unused)} unused of {len(defined_keys)} keys.")
     return unused
@@ -97,60 +97,121 @@ def sanitize_comment(text):
 
 def generate_strings(args):
     keys_and_strings = get_keys_and_strings_from_xcstrings(args.input)
+
     sorted_keys = sorted(keys_and_strings.keys())
 
-    swiftified_keys = {swiftify_key(k): k for k in sorted_keys}
+    # Split keys into categories based on the first part of the key
+    original_keys = {}
+    split_keys = {}
+    split_values = {}
+    for key in sorted_keys:
+        parts = key.split(".")
+        if len(parts) > 1:
+            first_part = parts[0]
+            if first_part not in split_keys:
+                split_keys[first_part] = []
+            newKey = ".".join(parts[1:])
+            split_keys[first_part].append(newKey)
+            split_values[newKey] = keys_and_strings[key]
+            original_keys[newKey] = key
+        else:
+            if "general" not in split_keys:
+                split_keys["general"] = []
+            split_keys["general"].append(key)
+            split_values[key] = keys_and_strings[key]
+            original_keys[key] = key
+
+    split_keys = {k: sorted(v) for k, v in split_keys.items()}
+
+    # Generate Swiftified keys to see if they are used in the code
+    swiftified_keys = []
+    for category, keys in split_keys.items():
+        for key in keys:
+            capitalized_category = category.capitalize()
+            swiftified_key = swiftify_key(key)
+            swiftified_keys.append(f"{capitalized_category}.{swiftified_key}")
+
+
     unused_keys = find_unused_keys(args, swiftified_keys)
 
     os.makedirs(os.path.dirname(args.output_swift), exist_ok=True)
 
+    # sort categories alphabetically
+    split_keys = dict(sorted(split_keys.items(), key=lambda item: item[0]))
+    # sort keys alphabetically
+    for category, keys in split_keys.items():
+        split_keys[category] = sorted(keys)
+
+    # Generate the Swift file
     with open(args.output_swift, "w", encoding="utf-8") as f:
+        # Write the header
         f.write("// swiftlint:disable all\n")
         f.write(f"// Generated from {os.path.basename(args.input)}\n")
         f.write(f"// Do not edit manually\n\n")
         f.write("import Foundation\n\n")
         f.write(f"public enum {args.enum_name}: Equatable, Hashable {{\n")
 
-        for key in sorted_keys:
-            swift_key = swiftify_key(key)
-            string_value = keys_and_strings[key]
-            types = extract_placeholder_types(string_value)
-            comment = f"  /// {sanitize_comment(string_value)}"
-            if swift_key in unused_keys:
-                comment += " // TODO: Unused"
-            f.write(f"{comment}\n")
-            if types:
-                f.write(f"  case {swift_key}({', '.join(types)})\n")
-            else:
-                f.write(f"  case {swift_key}\n")
+        for category, keys in split_keys.items():
+            f.write(f"  // MARK: - {category.capitalize()}\n")
 
+            # Write category enum
+            f.write(f" public enum {category.capitalize()} {{\n")
+
+            # Write keys
+            for key in keys:
+                swift_key = swiftify_key(key)
+                string_value = split_values[key]
+                types = extract_placeholder_types(string_value)
+                typesArg = []
+
+                if types and len(types) > 0:
+                    types = [typ.capitalize() for typ in types]
+                    typesArg = [f"p{idx}: {typ}" for idx, typ in enumerate(types)]
+                else:
+                    types = []
+                    typesArg = []
+
+                string_value_with_args = string_value
+                for idx, typ in enumerate(types):
+                    if typ == "String":
+                        string_value_with_args = re.sub(r"%@", f"\(p{idx})", string_value_with_args, count=1)
+                    elif typ == "Int":
+                        string_value_with_args = re.sub(r"%d", f"\(p{idx})", string_value_with_args, count=1)
+                    elif typ == "Double":
+                        string_value_with_args = re.sub(r"%f", f"\(p{idx})", string_value_with_args, count=1)
+                    elif typ == "Character":
+                        string_value_with_args = re.sub(r"%c", f"\(p{idx})", string_value_with_args, count=1)
+                    elif typ == "Int64":
+                        string_value_with_args = re.sub(r"%li", f"\(p{idx})", string_value_with_args, count=1)
+                    elif typ == "UInt":
+                        string_value_with_args = re.sub(r"%u", f"\(p{idx})", string_value_with_args, count=1)
+                    else:
+                        print(f"Unknown type {typ} for key {key}")
+                
+                comment = f"    /// {sanitize_comment(string_value)}"
+
+                unused_key = f"{category.capitalize()}.{swift_key}"
+                if unused_key in unused_keys:
+                    comment = f"    #warning(\"L10n.{unused_key} is unused\")\n" + comment
+                f.write(f"{comment}\n")
+                if types:
+                    f.write(f"    public static func {swift_key}({', '.join(typesArg)}) -> LocalizedStringResource {{\n")
+                    f.write(f"      LocalizedStringResource(\n")
+                    f.write(f"          \"{original_keys[key]}\",\n")
+                    f.write(f"          defaultValue: \"{string_value_with_args}\"\n")
+                    f.write(f"      )\n")
+                    f.write(f"    }}\n")
+                else:
+                    f.write(f"    public static let {swift_key} = LocalizedStringResource(\n")
+                    f.write(f"        \"{original_keys[key]}\",\n")
+                    f.write(f"        defaultValue: \"{string_value}\"\n")
+                    f.write(f"    )\n")
+                
+                if key != keys[-1]:
+                    f.write("\n")
+
+            f.write("  }\n\n")
         f.write("}\n\n")
-
-        f.write(f"extension {args.enum_name}: CustomStringConvertible {{\n")
-        f.write("  public var description: String { return self.string }\n\n")
-        f.write("  public var string: String {\n")
-        f.write("    switch self {\n")
-
-        for key in sorted_keys:
-            swift_key = swiftify_key(key)
-            types = extract_placeholder_types(keys_and_strings[key])
-            if types:
-                params = [f"p{i+1}" for i in range(len(types))]
-                pattern = ", ".join(f"let {p}" for p in params)
-                call_args = ", ".join(params)
-                f.write(f"    case .{swift_key}({pattern}):\n")
-                f.write(f'      return {args.enum_name}.tr(key: "{key}", {call_args})\n')
-            else:
-                f.write(f"    case .{swift_key}:\n")
-                f.write(f'      return {args.enum_name}.tr(key: "{key}")\n')
-
-        f.write("    }\n  }\n\n")
-
-        f.write("  private static func tr(key: String, _ args: CVarArg...) -> String {\n")
-        f.write("    let format = NSLocalizedString(key, comment: key)\n")
-        f.write("    return String(format: format, arguments: args)\n")
-        f.write("  }\n")
-        f.write("}\n")
 
     print(f"Generated {args.output_swift} with {len(sorted_keys)} keys.")
 
